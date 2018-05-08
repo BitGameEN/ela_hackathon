70a349f08fb3dc2782c0a6efa430230254d08b68
guli <guli@tsinghua.org.cn>
2018-05-08 16:22:21 +0800

    home

--- app/src/main/java/com/elastos/chat/ui/FriendsFragment.java
+++ app/src/main/java/com/elastos/chat/ui/FriendsFragment.java
@@ -21,6 +21,6 @@ public class FriendsFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        return inflater.inflate(R.layout.fragment_friends, container, false);
     }
 }
