821acd7dce61a4e2b011477a142929d0efcc8b6b
chao <515249207@qq.com>
2018-05-09 21:02:10 +0800

    feat:扫一扫初步

---
+++ app/src/main/java/com/elastos/chat/activity/ScanQRCodeActivity.java
@@ -0,0 +1,107 @@
+package com.elastos.chat.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import com.elastos.chat.R;
+import com.elastos.chat.ui.view.AppBar;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
+import java.util.List;
+
+import butterknife.BindView;
+
+/**
+ * @author rczhang on 2018/05/09.
+ */
+public class ScanQRCodeActivity extends BaseActivity {
+
+    private CaptureManager capture;
+    private CompoundBarcodeView barcodeScannerView;
+
+    @BindView(R.id.appbar) AppBar appBar;
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, ScanQRCodeActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        barcodeScannerView = initializeContent();
+        capture = new CaptureManager(this, barcodeScannerView);
+        barcodeScannerView.setStatusText("扫一扫，添加好友");
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+
+        barcodeScannerView.decodeContinuous(new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                barcodeScannerView.pause();
+                if (result.getResult().getBarcodeFormat() != BarcodeFormat.QR_CODE) {
+                    barcodeScannerView.resume();
+                    Toast.makeText(ScanQRCodeActivity.this, "没有发现二维码", Toast.LENGTH_SHORT).show();
+                } else {
+                    // TODO: 2018/5/9 添加好友
+                }
+            }
+
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {
+            }
+        });
+    }
+
+    /**
+     * Override to use a different layout.
+     *
+     * @return the CompoundBarcodeView
+     */
+    protected CompoundBarcodeView initializeContent() {
+        setContentView(R.layout.activity_scan_qr_code);
+        return (CompoundBarcodeView) findViewById(R.id.zxing_barcode_scanner);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+}
